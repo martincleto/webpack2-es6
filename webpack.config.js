56d8de412858b55df420d7e384042cbe50463654
Santiago Martín-Cleto <santiago.martin@condenast.es>
2017-01-24 16:12:52 +0100

    chore/improve-webpack-perfomance - Updating webpack & webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,14 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
-const basePath = '..';
+const basePath = './';
 const dir_src = path.resolve(__dirname, basePath, 'src')
 const dir_js = path.resolve(__dirname, dir_src, 'js');
 const dir_html = path.resolve(__dirname, dir_src, 'html');
 const dir_build = path.resolve(__dirname, basePath, 'build');
+
+let env = process.env.NODE_ENV || "development";
+let debug = ["development", "test"].indexOf(env) !== -1;
 
 module.exports = {
     entry: {
@@ -17,7 +20,7 @@ module.exports = {
         ]
     },
     output: {
-        filename: '[name].js',
+        filename: '[name].js?[hash]',
         path: dir_build
     },
     module: {
@@ -29,7 +32,7 @@ module.exports = {
                     path.resolve(__dirname, 'node_modules'),
                     dir_js + '/**/*.spec.js'
                 ],
-                options: {  
+                options: {
                     'plugins': ['transform-decorators-legacy'],
                     'presets': ['es2015']
                 }
@@ -37,12 +40,23 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
         new CopyWebpackPlugin([
-            { from: dir_html } // to: output.path
+            {
+              from: dir_html
+            } // to: output.path
         ])
     ],
     devtool: 'cheap-module-source-map',
     devServer: {
-        contentBase: dir_build
+      contentBase: dir_build,
+      hot: true,
+      stats: {  // @see https://webpack.js.org/configuration/stats/
+        assets: true,
+        errors: true,
+        errorDetails: true,
+        timings: true,
+        warnings: true
+      }
     }
 };
