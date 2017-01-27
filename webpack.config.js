49a9f749704508e773c01c313ad986f07f1954f6
Santiago Martín-Cleto <santiago.martin@condenast.es>
2017-01-27 13:49:29 +0100

    feature/sass-support - SASS initial set-up

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,24 @@
 
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
 const path = require('path');
 
-const env = process.env.NODE_ENV || 'development';
 const basePath = './';
-const dir_src = path.resolve(__dirname, basePath, 'src')
-const dir_js = path.resolve(__dirname, dir_src, 'js');
-const dir_html = path.resolve(__dirname, dir_src, 'html');
-const dir_build = path.resolve(__dirname, basePath, 'build');
+const dir_src = path.resolve(__dirname, basePath, 'src');
+const dir_src_html = path.resolve(__dirname, dir_src, 'html');
+const dir_src_js = path.resolve(__dirname, dir_src, 'js');
+const dir_src_sass = path.resolve(__dirname, dir_src, 'sass');
+const dir_build = path.resolve(basePath, 'build');
+const env = process.env.NODE_ENV || 'development';
 
 let baseConfig = {
     entry: {
-        main: path.resolve(dir_js, 'main.js'),
+        main: [
+          path.join(dir_src_js, 'main.js'),
+          path.join(dir_src_sass, 'styles.scss')
+        ],
         vendor: [
             'babel-polyfill'
         ]
@@ -30,21 +35,29 @@ let baseConfig = {
                 loader: 'babel-loader',
                 exclude: [
                     path.resolve(__dirname, 'node_modules'),
-                    dir_js + '/**/*.spec.js'
+                    dir_src_js + '/**/*.spec.js'
                 ],
                 options: {
                     'plugins': ['transform-decorators-legacy'],
                     'presets': ['es2015']
                 }
+            },
+            {
+                test: /\.(scss)$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
             }
         ]
     },
     plugins: [
         new CopyWebpackPlugin([
             {
-              from: dir_html
-            } // to: output.path
-        ])
+              from: dir_src_html
+            }
+        ]),
+        new ExtractTextPlugin({ // define where to save the file
+             filename: 'css/styles.css?[hash]',
+             allChunks: true
+        }),
     ]
 };
 let envConfig = require(`./config/webpack.config.${env}.js`);
